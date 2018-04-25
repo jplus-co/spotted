87cc7f5a65d17b62fd69d86b560c02f4fd51ba46
mikehwagz <hmiklw@me.com>
2018-04-25 11:23:49 -0400

    about page desktop and mobile

---
+++ src/scripts/views/about.js
@@ -0,0 +1,15 @@
+import Barba from 'barba.js'
+
+const about = Barba.BaseView.extend({
+  namespace: 'about',
+
+  onEnter() {},
+
+  onEnterCompleted() {},
+
+  onLeave() {},
+
+  onLeaveCompleted() {},
+})
+
+export default about
