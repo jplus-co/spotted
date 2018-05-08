7ff04436d7d863113d28bd95643e6b560352d3d5
mikehwagz <hmiklw@me.com>
2018-05-07 23:02:54 -0400

    simplify wp page template logic

--- src/page.php
+++ src/page.php
@@ -3,4 +3,4 @@
 $context = Timber::get_context();
 $context['post'] = new TimberPost();
 
-Timber::render('page.twig', $context);
+Timber::render(array('page-' . $post->post_name . '.twig', 'page.twig'), $context);
